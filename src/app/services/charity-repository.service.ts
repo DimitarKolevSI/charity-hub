4f0c828d65da03ae67e8708e689f35dc8451c0c9
Димитър Колев <dimitarkolev0911@gmail.com>
2021-05-27 23:34:06 +0300

    Added new test charities

--- src/app/services/charity-repository.service.ts
+++ src/app/services/charity-repository.service.ts
@@ -9,34 +9,7 @@ import {Donation} from '../models/Donation';
 export class CharityRepositoryService {
 
   private static idCounter: number = 0;
-  private static charities: Charity[] = [
-    {
-      id: CharityRepositoryService.idCounter++,
-      title: 'Test charity 1',
-      description: 'This is test data',
-      volunteersNeeded: 0,
-      volunteers: 0,
-      moneyNeeded: 1000,
-      moneyDonated: 0,
-      creatorsUsername: 'dimitarkolev0911',
-      imageUrl: 'https://th.bing.com/th/id/OIP.maFQtN1HV5qTR5CEG3Rb6AHaE8?w=268&h=180&c=7&o=5&dpr=2&pid=1.7',
-      donations: [],
-      volunteersUsernames: []
-    },
-    {
-      id: CharityRepositoryService.idCounter++,
-      title: 'Test charity 2',
-      description: 'This is also test data',
-      volunteers: 0,
-      volunteersNeeded: 20,
-      moneyNeeded: 0,
-      moneyDonated: 0,
-      creatorsUsername: 'ivana99',
-      imageUrl: 'https://th.bing.com/th/id/R17444093a813b069e51bd40aa975030f?rik=BfG%2fZ%2bl7yyVYkw&pid=ImgRaw',
-      donations: [],
-      volunteersUsernames: []
-    }
-  ];
+  private static charities: Charity[] = CharityRepositoryService.init();
 
   constructor() {
   }
@@ -143,6 +116,44 @@ export class CharityRepositoryService {
 
   hasVolunteerByUsername(charity: Charity, username: string): boolean {
     return charity.volunteersUsernames.filter(user => user === username).length > 0;
+  }
+
+  private static init(): Charity[] {
+    return [
+      {
+        id: CharityRepositoryService.idCounter++,
+        title: 'Save the Oceans',
+        description: 'The ocean is a planetary superpower. Home to spectacular ecosystems and treasured wildlife, the ocean covers 71% of our Earth’s surface and sustains the lives of billions of people. It regulates our climate, produces half the oxygen we breathe, and fuels the water cycle that produces rain and freshwater.' +
+          'Studies show that when ocean ecosystems are resilient, people nearby are more resilient too. A group of researchers looked at more than 100 studies to better understand the connection between marine protection and human health, and found that a majority of people realize the social, health, economic, governance, and ecological benefits from a healthy ocean. But those benefits are not guaranteed.' +
+          'After decades of overuse and pollution, these services are being interrupted.' +
+          'One out of three fish stocks is overfished. Hundreds of thousands of marine mammals, seabirds, and sea turtles are captured each year, along with tens of millions of sharks. Half of all coral reefs and mangroves are gone.' +
+          'We still have time to give the ocean the room it needs to be resilient in the face of growing threats. But we have to work together. WWF is prioritizing two pathways to build a more resilient ocean—We’re finding the solutions to fix broken global systems that lead to declines in nature while protecting those places that wildlife and people can’t live without.',
+        volunteersNeeded: 0,
+        volunteers: 0,
+        moneyNeeded: 10_000_000,
+        moneyDonated: 0,
+        creatorsUsername: 'dimitarkolev0911',
+        imageUrl: 'https://c402277.ssl.cf1.rackcdn.com/photos/10521/images/portrait_overview/MID_104447.jpg?1449605159',
+        donations: [],
+        volunteersUsernames: []
+      },
+      {
+        id: CharityRepositoryService.idCounter++,
+        title: 'Waterkeeper Alliance',
+        description: 'Waterkeeper Alliance fights for all people in every community to have clean water for drinking, swimming, and fishing safely.' +
+          'The alliance is 100% focused on clean water and works through enabling and connecting grassroots organizations and affiliates in this cause. In this mission, they also seek to stop water polluters, such as coal companies and hog farms, from fouling the water.' +
+          'The concept of Waterkeepers started its work with the Hudson River in New York in 1966. Fishermen banded together to stop industrial pollution from destroying their livelihoods. The movement resulted in the Hudson recovering, and the success inspired more waterkeepers around the country and the world.' +
+          'Now, Waterkeeper Alliance bands over 300 organizations striving to protect over 2.5 million square miles of waterways, including rivers, lakes, and coastlines. Efforts stretch from Alaska to Australia.',
+        volunteers: 0,
+        volunteersNeeded: 1_000,
+        moneyNeeded: 1_000_000,
+        moneyDonated: 0,
+        creatorsUsername: 'ivana99',
+        imageUrl: 'https://greenglobaltravel.com/wp-content/uploads/2016/11/Waterkeeper-Alliance.jpg',
+        donations: [],
+        volunteersUsernames: []
+      }
+    ];
   }
 
 }
